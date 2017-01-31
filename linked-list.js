06ff805513da2d4d0710e4b13941e87527c4dc5b
Alex Bokii <vanilinka33@gmail.com>
2017-01-31 21:34:15 +0000

    Linked list - implementation of add and print methods

---
+++ linked-list.js
@@ -0,0 +1,51 @@
+class LinkedList {
+    constructor(head, tail) {
+        this.head = head;
+        this.tail = tail;
+    }
+
+    add(value) {
+        if(this.head == null) {
+            this.head = {'value': value, 'next': null};
+            this.tail = this.head;
+        }
+        else {
+            let tailNext = {'value': value, 'next': null};
+            this.tail.next = tailNext;
+            this.tail = tailNext;
+        }
+    }
+
+    print() {
+        let current = this.head;
+        while(current != null) {
+            console.log(current);
+            current = current.next;
+        }
+    }
+
+    search(val) {
+        let current = this.head;
+        let result;
+        while(current != null) {
+            if(current.value === val) {
+                result = current;
+            }
+            current = current.next;
+        }
+        return result; 
+    }
+}
+
+let myLinkedList = new LinkedList;
+myLinkedList.add("Hello");
+myLinkedList.add("my");
+myLinkedList.add("name");
+myLinkedList.add("is");
+myLinkedList.add("Sun");
+
+myLinkedList.print();
+
+myLinkedList.search("my");
+myLinkedList.search("5");
+
